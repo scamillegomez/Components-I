34127dd432006c40217bb12a61830bb32315235f
scgomez <sydneycamille0896@gmail.com>
2023-07-01 19:59:22 -0500

    completed assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 import './index.less'
 import './components/article/article.js'
 import './components/menu/menu.js'
+
+// menuDiv = document.createElement('div');
+// menuList = document.createElement('ul');
+
+// header = document.querySelector('.header');
+// header.appendChild(menuDiv);
+// menuDiv.appendChild(menuList);
+
+// menuDiv.classList.add('menu');
+
+
+// menuData.forEach(element=>{
+//     var listItem = document.createElement('li');
+//     listItem.textContent = element;
+//     menuDiv.appendChild(listItem);
+//     return listItem;
+// });
+
+
+
